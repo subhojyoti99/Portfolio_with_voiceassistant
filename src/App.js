3191d22dc37d2e1bc60450ebc5664af3c53b7511
SUBHOJYOTI SINGHA <subhajyotisingha@gmail.com>
2024-11-22 03:35:02 +0530

    route modified

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route
-          path="/"
+          path="Portfolio_with_voiceassistant/"
           element={
             <>
               <Welcome />
